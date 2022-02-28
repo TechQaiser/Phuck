1a3b8723adf85e9989a9517a9cc391559e08c9d0
Qaiser Abbas <69212320+TechQaiser@users.noreply.github.com>
2022-02-28 23:12:36 +0500

    Add files via upload

--- Phuck.py
+++ Phuck.py
@@ -1,2 +1,14 @@
-import Phuck
-Phuck.__techq()
+import os, platform
+try:
+   import requests
+except:
+   os.system('pip2 install requests')
+ 
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import Phuck
+    Phuck. __techq()
+elif bit == '32bit':
+    import Phuck
+    Phuck.__techq()
